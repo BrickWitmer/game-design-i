fda4c21b34ac41c270b729b28da6093458e0f5e9
BrickWitmer <witmer.b@stu.janesville.k12.wi.us>
2024-09-05 14:24:06 -0500

    Update 9/5

---
+++ Langdocs/prog_52a.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+func _on_btn_calc_pressed() -> void:
+	var Length = int($txtLen.text)
+	var Width = int($txtWid.text)
+	var Area = Length * Width
+	var Perim = 2 * Length + 2 * Width
+	$lblArea.text = "Area: " + str(Area)
+	$lblPeri.text = "Perimeter: " + str(Perim)
+	#str - string (text)
+	#int - interger (whole number)
+	#float - floating point number (w/ decimal)
+
+
+
+func _on_btn_clear_pressed() -> void:
+	$lblArea.text = "Area: "
+	$lblPeri.text = "Perimeter: "
+	$txtLen.text = ""
+	$txtWid.text = ""
+
+
+func _on_btn_exit_pressed() -> void:
+	get_tree().quit()

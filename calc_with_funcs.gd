cbf113861525124ad078c3dab2477cefa093d703
BrickWitmer <witmer.b@stu.janesville.k12.wi.us>
2024-10-01 14:36:54 -0500

    update 10/1

---
+++ calc_with_funcs.gd
@@ -0,0 +1,15 @@
+extends Control
+
+var num1 = int($LineEdit.text)
+var num2 = int($LineEdit2.text)
+var answer
+
+func myAdd(num1, num2):
+	num1 + num2
+func myMul(num1,num2):
+	num1 * num2
+func mySub(num1,num2):
+	num1 - num2
+func myPower(num1,num2):
+	num1 ** num2
+func myDivide(num1,num2)

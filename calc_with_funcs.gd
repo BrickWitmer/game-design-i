f4d6788ccba6d069aba8e9430777b1ef62c85310
BrickWitmer <witmer.b@stu.janesville.k12.wi.us>
2024-10-03 12:31:04 -0500

    update 10/3

--- calc_with_funcs.gd
+++ calc_with_funcs.gd
@@ -1,15 +1,38 @@
 extends Control
 
+var Equation
 var num1 = int($LineEdit.text)
 var num2 = int($LineEdit2.text)
-var answer
+var Ans
 
 func myAdd(num1, num2):
-	num1 + num2
+	Equation = "Add"
+	Ans = num1 + num2
 func myMul(num1,num2):
-	num1 * num2
+	Equation = "Multiply"
+	Ans =num1 * num2
 func mySub(num1,num2):
-	num1 - num2
+	Equation = "Subtract"
+	Ans = num1 - num2
 func myPower(num1,num2):
-	num1 ** num2
-func myDivide(num1,num2)
+	Equation = "Power"
+	Ans = num1 ** num2
+func myDivide(num1,num2):
+	Equation = "Divide"
+	Ans = num1 / num2
+func mod(num1,num2):
+	Equation = "Mod"
+	Ans = num1 % num2
+
+func _on_btn_eq_pressed() -> void:
+	if $btnP.pressed:
+		myAdd(num1,num2)
+	if $btnM.pressed:
+		mySub(num1,num2)
+	if $btnMu.pressed:
+		myMul(num1,num2)
+	if $btnDiv.pressed:
+		myDivide(num1,num2)
+	if $btnMod.pressed:
+		mod(num1,num2)
+	$lblOp2.text = str(Ans)

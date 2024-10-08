42956c4e533d3dcc81423fdb2a6755820349f3b6
BrickWitmer <witmer.b@stu.janesville.k12.wi.us>
2024-10-08 14:21:53 -0500

    update 10/8

--- plt_player.gd
+++ plt_player.gd
@@ -3,16 +3,23 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var gravity_on = true
+var double_jump = true
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() and gravity_on:
 		velocity += get_gravity() * delta
+	if is_on_floor():
+		double_jump = true
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept") and not is_on_floor() and double_jump == true:
+		velocity.y = JUMP_VELOCITY 
+		double_jump = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,8 +30,25 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	update_animation(direction)
-
-
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		gravity_on = not gravity_on
+	if Input.is_action_just_pressed("ui_home"):
+		self.scale *= 2
+	if Input.is_action_just_pressed("ui_end"):
+		self.scale /= 2
+	if Input.is_action_just_pressed("ui_page_up"):
+		self.rotation_degrees += 5
+	if Input.is_action_just_pressed("ui_page_down"):
+		self.rotation_degrees -= 5
+	if Input.is_action_just_pressed("ui_text_completion_replace"):  #tab
+		self.position.x += 500
+	if Input.is_action_just_pressed("ui_text_backspace"):
+		self.position.x -= 500
+	if Input.is_action_just_pressed("ui_text_toggle_insert_mode"):
+		self.position.y += JUMP_VELOCITY / 2.0
+	if Input.is_action_just_pressed("ui_text_delete"):
+		self.position.y -= JUMP_VELOCITY / 2.0
 
 	move_and_slide()
 

571ba76a43e537f7a8bb49d27fb30c2dc817590b
BrickWitmer <witmer.b@stu.janesville.k12.wi.us>
2024-10-31 14:36:51 -0500

    update 10_31

--- Zelda_like/td_player.gd
+++ Zelda_like/td_player.gd
@@ -1,7 +1,18 @@
 extends CharacterBody2D
 
+const SPEED = 100.0
+const MAX_OBTAINABLE_HEALTH = 400
+enum STATES {IDLE=0, DEAD, DAMAGED, ATTACKING, CHARGING}
 
-const SPEED = 300.0
+@export var data = {
+	"max_health": 60.0,
+	"health": 60.0,
+	"money": 0,
+	"state": STATES.IDLE,
+	"secondaties": []
+}
+
+
 var inertia = Vector2()
 var look_direction = Vector2.DOWN
 
@@ -18,9 +29,25 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED) #Smooth player down to zero speed
 	velocity += inertia
+	update_animation(direction)
 	move_and_slide()
 	inertia = inertia.move_toward(Vector2.ZERO, delta * 1000) # Make Inertia Zero
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		$Camera2D/Pause_menu.show()
 		get_tree().paused = true
+
+func update_animation(direction):
+	var a_name = "idle_"
+	if direction.length() > 0:
+		a_name = "walk_"
+	if look_direction.x != 0:
+		a_name += "side"
+		$Animated2dSprite.flip_h = look_direction.x < 0
+	elif look_direction.y < 0:
+		a_name += "up"
+	elif look_direction.y > 0:
+		a_name += "down"
+	$Animated2dSprite.animation = a_name
+	$Animated2dSprite.play()
+	pass

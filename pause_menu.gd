6ea349a58ed877e4a69c1f0c236ea8f84fdeec3d
BrickWitmer <witmer.b@stu.janesville.k12.wi.us>
2024-12-03 14:34:37 -0600

    Update 12/3

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var player = get_tree().get_first_node_in_group("Player")
+@onready var p_HUD = get_tree().get_first_node_in_group("HUD")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.process_mode = Node.PROCESS_MODE_ALWAYS
@@ -8,13 +9,13 @@ func _ready() -> void:
 func _on_resume_pressed() -> void:
 	self.hide()
 	get_tree().paused = false
-	player.data.max_health = 60
-	player.data.state = player.STATES.IDLE
-	player.data.health = player.data.max_health
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 
 func _on_restart_pressed() -> void:
+	player.data.health = 60
+	player.data.state = player.STATES.IDLE
 	get_tree().reload_current_scene()
+	
